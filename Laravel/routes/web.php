91ed2bcebb747db397ce56fd9c23a147df043e9e
unknown <rossijulia1999@gmail.com>
2025-10-03 21:38:51 -0300

    laravel login/registro con estilizado
    tambien un primer intento de landing a mejorar

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('welcome');
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('landing', function () { return Inertia::render('landing'); })->name('landing');
 });
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
